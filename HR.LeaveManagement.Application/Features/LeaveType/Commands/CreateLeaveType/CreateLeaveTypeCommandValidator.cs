b3e2462795e8c9fa5df0aab522850d84f926d1d9
Alexej Pitkowski <alexej.pitkowski@gmail.com>
2024-04-23 11:40:25 +0200

    Setting up the Application Core

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType
+{
+    public class CreateLeaveTypeCommandValidator : AbstractValidator<CreateLeaveTypeCommand>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public CreateLeaveTypeCommandValidator(ILeaveTypeRepository leaveTypeRepository)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("{PropertyName} is required")
+                .NotNull()
+                .MaximumLength(70).WithMessage("{PropertyName} must be fewer than 70 characters");
+
+            RuleFor(x => x.DefaultDays)
+                .LessThan(100).WithMessage("{PropertyName} cannot exceed 100")
+                .GreaterThanOrEqualTo(1).WithMessage("{PropertyName} cannot be lees than 1");
+
+            RuleFor(x => x)
+                .MustAsync(LeaveTypeNameUnique).WithMessage("Leave type already exists");
+        }
+
+        private async Task<bool> LeaveTypeNameUnique(CreateLeaveTypeCommand command, CancellationToken token)
+        {
+            return await _leaveTypeRepository.IsLeaveTypeUniqueAsync(command.Name);
+        }
+    }
+}
